54244d0a8ad9ca851c41aafa38fc5a5edbbe35e0
tomzhangv <tom_zhangv@163.com>
2022-03-04 17:54:04 +0800

    2022/3/4 spring code

--- spring-demo/src/main/java/com/mjp/MyApplication.java
+++ spring-demo/src/main/java/com/mjp/MyApplication.java
@@ -2,8 +2,9 @@ package com.mjp;
 
 import com.mjp.bean.Car;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
+@ComponentScan("com.mjp.bean")
 @Configuration
 public class MyApplication {
 	@Bean(value = "car")
--- spring-demo/src/main/java/com/mjp/test/Test.java
+++ spring-demo/src/main/java/com/mjp/test/Test.java
@@ -1,7 +1,9 @@
 package com.mjp.test;
 
 import com.mjp.MyApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 	public static void main(String[] args) {
@@ -10,6 +12,16 @@ public class Test {
 		for (String beanDefinitionName : beanDefinitionNames) {
 			System.out.println(beanDefinitionName);
 		}
+
+		Object car = context.getBean("car");
+		System.out.println("beanDefinitionName:"+car);
+
+		System.out.println("********************************************************");
+		ApplicationContext contex=new ClassPathXmlApplicationContext("tx.xml");
+		Object car1 = contex.getBean("car");
+		System.out.println("car****:"+car1);
+
+
 	}
 }
 
